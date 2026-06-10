ecc88b24361f2f529c45bf981e5582a2ae3fafcc
Ncl-cmd <claude.nigot@gmail.com>
2026-06-10 17:47:18 +0200

    Python data, loop,... test included

--- 00 Pattern Exercises/retest.py
+++ 00 Pattern Exercises/retest.py
@@ -6,16 +6,16 @@ llist= []
 ttup = ()
 print(type(ttup))
 for i in range(l):
+    print(i,end=" ")
     dic[i]=s[i]
     sset.add(s[i])
     llist.append(s[i])
     ttup += (s[i],)
-for i in range(l):
-    dic[i]=s[i]
-    sset.add(s[i])
-    llist.append(s[i])
-    ttup += (s[i],)
-print(dic)
-print(sset)
-print(llist)
-print(ttup)
+print()
+print("dict:  ",dic)
+print("set:   ",sset)
+print("list:  ",llist)
+print("tuple: ",ttup)
+for i in range(5,0,-1):
+    print(i,end=" ")
+print()
